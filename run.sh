41160d16ff8715e3edfe9521e66b458251a5b5fe
Vinicius Andrade <vinicius.andrade00@gmail.com>
2023-11-28 05:20:36 -0300

    tests run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,17 @@ export PATH=$PATH:$HOME/.dotnet
 export DOTNET_ROOT=$HOME/.dotnet
 export LOGGING__LOGLEVEL__DEFAULT=Information
 export LOGGING__LOGLEVEL__MICROSOFT=Warning
-CONFIG_PATH=/data/options.json
+export CONFIG_PATH=/data/options.json
 
 set_environment_variable() {
     local env_variable_name="$1"
     local config_name="$2"
 
     local config_value="$(bashio::config "$config_name")"
+    # print config value to log but not to stdout
+    echo "$config_name is set to $config_value" >&2
 
+    
     if [ -n "$config_value" ]; then
         # Variable is defined and not empty
         export "$env_variable_name"="$config_value"
